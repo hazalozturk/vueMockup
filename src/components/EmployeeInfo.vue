9ee9f7ec26ac81143c22d90294d6db823c31e4bc
hazalozturk <hcanisag@gmail.com>
2018-12-04 18:42:59 -0500

    create EmployeeInfo component that shows image, name and title information

---
+++ src/components/EmployeeInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex
+        v-for="n in 32"
+        :key="n"
+        xs3>
+        <v-flex mr-4>
+        <v-img
+          :src="`https://picsum.photos/300/300?image=${n}`"
+        ></v-img>
+        </v-flex>
+        <v-flex mb-4>
+          <h1><a
+            href=''
+            target="_blank">
+              Name
+          </a></h1>
+          <p><a
+            href=''
+            target="_blank">
+              Title
+          </a></p>
+        </v-flex>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
