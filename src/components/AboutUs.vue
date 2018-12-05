f2eacc716bb451ff1e228af700f3824bdbd22573
hazalozturk <hcanisag@gmail.com>
2018-12-05 13:26:29 -0500

    make the component responsive

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid px-0 py-0>
+  <v-container fluid px-0 py-0 text-xs-center grid-list-md>
     <v-flex xs12 grey lighten-3>
       <img src="../assets/banner.jpg">
     </v-flex>
@@ -7,10 +7,10 @@
       <h1 class="deep-orange--text pb-4 pt-5 text-xs-center font-weight-bold">
         { ABOUT  TROY WEB CONSULTING }
       </h1>
-      <h3 class="text-xs-center font-weight-medium pb-2">
+      <h3 class="font-weight-medium pb-2">
         Our Mission Is To Design And Build Kick-Ass Software.
       </h3>
-      <p class="text-xs-center font-weight-thin mx-5 px-5 pb-5 mb-5">
+      <p class="font-weight-thin mx-5 px-5 pb-5 mb-5">
         In 2002, Troy Web Consulting formed from a small band of software
         developers in New York's Capital District who had convened around
         various rapid application development tools that were beginning to
@@ -27,16 +27,22 @@
         Modernization and Consulting & Strategy. </p>
     </v-flex>
     <v-flex xs12>
-      <h2 class="deep-orange--text py-4 text-xs-center font-weight-bold">
+      <h2 class="deep-orange--text py-4 font-weight-bold">
         { OUR CORE VALUES }
       </h2>
-      <v-layout justify-center>
-          <img height="100" src="https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec5658898583febaffd9a9/1542215260295/CoreValues-01-min.png?format=300w">
-          <img height="100" src="https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec566640ec9a4b55d1ebc1/1542215277529/CoreValues-04-min.png?format=300w">
-          <img height="100" src="https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec56741ae6cf499c60da68/1542215292529/CoreValues-02-min.png?format=300w">
-          <img height="100" src="https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec568421c67c90311b61d6/1542215307872/CoreValues-03-min.png?format=300w">
-          <img height="100" src="https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec56930e2e72982defdf07/1542215322567/CoreValues-05-min.png?format=300w">
-          <img height="100" src="https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec56a0898583febaffde1f/1542215335469/CoreValues-06-min.png?format=300w">
+      <v-layout justify-center row wrap>
+        <v-flex xs2 v-for="value in core_values">
+          <v-flex>
+          <v-img
+            :src="value.icon"
+          ></v-img>
+          </v-flex>
+          <v-flex>
+            <p class="font-weight-bold grey--text">
+              {{value.info}}
+            </p>
+          </v-flex>
+        </v-flex>
       </v-layout>
     </v-flex>
   </v-container>
@@ -44,5 +50,35 @@
 
 <script>
 export default {
+  data() {
+    return {
+      core_values: [
+        {
+          icon:'https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec5658898583febaffd9a9/1542215260295/CoreValues-01-min.png?format=300w',
+          info:'Be Agile, Flexible and Lean'
+        },
+        {
+          icon:'https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec566640ec9a4b55d1ebc1/1542215277529/CoreValues-04-min.png?format=300w',
+          info:'Embrace Change'
+        },
+        {
+          icon:'https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec56741ae6cf499c60da68/1542215292529/CoreValues-02-min.png?format=300w',
+          info:'Pay Attention to Details'
+        },
+        {
+          icon:'https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec568421c67c90311b61d6/1542215307872/CoreValues-03-min.png?format=300w',
+          info:'Lead by Example'
+        },
+        {
+          icon:'https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec56930e2e72982defdf07/1542215322567/CoreValues-05-min.png?format=300w',
+          info:'Give Back'
+        },
+        {
+          icon:'https://static1.squarespace.com/static/593ad95da5790a81949b5613/t/5bec56a0898583febaffde1f/1542215335469/CoreValues-06-min.png?format=300w',
+          info:'Kick-Ass and Take Names'
+        }
+      ]
+    }
+  }
 }
 </script>
